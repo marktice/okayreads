fbcba2b080ca6fedfdecfabecc2dacdae2fbd800
Mark Tice <marktice@protonmail.com>
2018-04-23 18:05:09 +1000

    Seed a admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
-10.times do
-  okbook = Book.create!(
-    title: Faker::Book.title,
-    author: Faker::Book.author,
-    description: Faker::Lorem.paragraph,
-    genre: Faker::Book.genre,
-    year: rand(1960..2018)
-  )
-  puts "Created #{okbook.title}"
-end
+# User.create!(
+#   email: 'markthomastice@gmail.com',
+#   password: 'password123',
+#   admin: true
+# )
+
+# 10.times do
+#   okbook = Book.create!(
+#     title: Faker::Book.title,
+#     author: Faker::Book.author,
+#     description: Faker::Lorem.paragraph,
+#     genre: Faker::Book.genre,
+#     year: rand(1960..2018)
+#   )
+#   puts "Created #{okbook.title}"
+# end
