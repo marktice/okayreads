2c0f9c0540065b310bb0ef0e60b70a809f87d13e
Mark Tice <marktice@protonmail.com>
2018-04-23 20:13:24 +1000

    Add book creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.order('author ASC')
   end
 
   def show
@@ -8,18 +8,28 @@ class BooksController < ApplicationController
   end
 
   def new
-    
+    @book = Book.new
   end
 
   def create
-    
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book created"
+      redirect_to books_path
+    else
+      flash[:alert] = "Could not save Book"
+      render :new
+    end
   end
 
   def update
-    
   end
 
   def destroy
-    
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :genre, :year)
   end
 end
